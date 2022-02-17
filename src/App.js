19fa0fc0b78486e961853c962cad4983e8bb8f6f
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-16 19:39:13 -0500

    create a separate ImageCard component, fetch data , and populate the card with data

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
+import { ImageCard } from "./components/ImageCard";
+import { useEffect, useState } from "react";
+
 export default function App() {
+	const [images, setImages] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [term, setTerm] = useState("");
+
+	useEffect(() => {
+		fetch(
+			`https://pixabay.com/api/videos/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=yellow+flowers&pretty=true`
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				setImages(data.hits);
+				setIsLoading(false);
+			})
+			.catch((error) => console.log(error));
+	}, []);
+
 	return (
-		<div className="max-w-sm rounded overflow-hidden shadow-lg">
-			<img src="https://source.unsplash.com/random" alt="" className="w-full" />
-			<div className="px-6 py-4">
-				<div className="font-bold text-purple-500 text-xl mb-2">
-					{" "}
-					Random photo
-				</div>
-				<ul>
-					<li>
-						<strong>Views: </strong>
-						400
-					</li>
-					<li>
-						<strong>Likes : </strong>
-						400
-					</li>
-					<li>
-						<strong>Views: </strong>
-						400
-					</li>
-				</ul>
-			</div>
-			<div className="px-6 py-4">
-				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-					#tag1
-				</span>
-				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-					#tag2
-				</span>
-				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-					#tag3
-				</span>
+		<div className="container mx-auto">
+			<div className="grid grid-cols-3 gap-4">
+				{images.map((image) => (
+					<ImageCard key={image.id} image={image} />
+				))}
 			</div>
 		</div>
 	);
